8e696eba3330d68628d88434c3e008dc10c7ee38
mkdev <aa019@juyoun.kim>
2019-11-14 09:46:54 +0000

    styled component added and css removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import {
   BrowserRouter as Router
 } from 'react-router-dom';
 import Routes from './Routes';
+// import styled from 'styled-components';
 
 const App: React.FC = () => {
+//	const img = styled.img`
+//		width: 10px;
+//	`
   return (
     <Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
+        <img src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
